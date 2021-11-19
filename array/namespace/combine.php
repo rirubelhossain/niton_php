4044c1bc380698ceb032cd5db3cf99a0a9687e8f
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-11-19 18:30:24 +0600

    Anonymous function

--- array/namespace/combine.php
+++ array/namespace/combine.php
@@ -21,4 +21,87 @@ echo "<br>";
 
 new namespace\j\Add();
 
+/// Super global variable in php
+echo "<br>";
+print_r($_ENV);
+echo "<br>";
+print_r($_COOKIE);
+echo "<br>";
+print_r($_POST);
+echo "<br>";
+print_r($_GET);
+echo "<br>";
+var_dump($_GET);
+echo "<br>";
+print_r($_REQUEST);
+echo "<br>";
+
+echo "<br>";
+//print_r($_SESSION);
+echo "<br>";
+/// How to use unset() in program
+
+$topics_list = ["Fruits" , "Animal", "Fish", "Birds"] ;
+print_r($topics_list);
+echo "<br>";
+unset($topics_list[2]);
+echo "<br>";
+print_r($topics_list);
+echo "<br>";
+unset($topics_list);
+var_dump($topics_list);
+echo "<br>";
+
+$topics_list = ["Fruits" , "Animal" , "Firsh" , "Birds"];
+print_r($topics_list);
+
+$count = count($topics_list);
+echo "<br>";
+echo $count. "<br>" ;
+
+for($i = 0 ; $i < $count ; $i++ ){
+    unset($topics_list[$i]);
+}
+print_r($topics_list);
+echo "<br>";
+var_dump($topics_list);
+
+$sum_ar = [1,2,3,4,5];
+$count_s = count($sum_ar) ;
+$sum = 0 ;
+echo "<br>";
+for($i = 0 ; $i < $count_s ; ++$i){
+    $sum += $sum_ar[$i] ;
+}
+echo "The summation is = ". $sum ."<br>";
+
+/// How to use foreach loop in array
+$grades = ['CSE101' => 'A+',
+            'ENG101' => 'B+' ,
+            'MAT101'=> 'A+' ,
+            'ENV101' => 'A+'
+];
+
+foreach($grades as $grades => $value){
+    echo "$grades : $value ". "<br>" ;
+}
+/* 
+sett(10);
+function addd($a){
+    return ($a * $a);
+}
+function sett(callable $addd){
+    echo $addd ;
+}
+*/
+
+echo "<br>";
+
+
+$anonumas = function ($kk):int{
+    return($kk * $kk) ;
+};
+echo "The result of the anonumus function = ", $anonumas(12,12);
+
+
 ?>
