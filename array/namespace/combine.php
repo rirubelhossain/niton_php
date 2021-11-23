5c97eda28eb1bcb85c13cbb3e3deaed9175ae225
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-11-24 02:38:46 +0600

    Namespace

--- array/namespace/combine.php
+++ array/namespace/combine.php
@@ -49,7 +49,7 @@ echo "<br>";
 print_r($topics_list);
 echo "<br>";
 unset($topics_list);
-var_dump($topics_list);
+//var_dump($topics_list);
 echo "<br>";
 
 $topics_list = ["Fruits" , "Animal" , "Firsh" , "Birds"];
@@ -102,6 +102,127 @@ $anonumas = function ($kk):int{
     return($kk * $kk) ;
 };
 echo "The result of the anonumus function = ", $anonumas(12,12);
+/// how to use clouser function 
+echo "<br><br>";
+$counter = 100 ;
+// change the counter here 
+$clouser = function () use (&$counter){
+     $counter++ ;
+};
+
+/// call the anonymous functions
+$clouser() ;
+$clouser() ;
+$clouser() ;
+
+echo "the result will be " , $counter. "<br>" ;
+
+/// how to use recursive function in program
+
+
+$sum_range = recursive_sum(100) ;
+
+echo "The output will be = ", $sum_range ;
+
+
+function recursive_sum($value ):int{
+    static $h = 0 ;
+    if( $value == 1){
+        return(1);
+    }else{
+        $h =  ($value + recursive_sum($value-1));
+        //echo $h . "<br>";
+    }
+    return $h ;
+}
+
+/// PHP string manipulation in program
+
+echo "<br>";
+$age = 10 ;
+echo 'The value of $age'  , "<br>"; /// All variables are will be string in this line !!
+
+echo "The value of $age", "<br>" ; /// All variables are will be printed in the line !!
+
+$age = 20 ;
+$name = "Rubel Hossain";
+$str = "Hi my name is {$name} and my age is {$age}"."<br>";
+
+echo $str . "<br>";
+
+function summation(){
+    return(10 * 10 );
+}
+
+echo "Hi my name is {$name} and my age is {$age} and some calculation is {summation()}";
+echo "<br>";
+
+echo date_default_timezone_get() , "<br>";
+//echo date_sunrise(), "<br>";
+
+
+echo decbin(15) , "<br>";
+echo decoct(15), "<br>";
+echo dechex(15), "<br>";
+
+///exption handeling 
+echo "<br>";
+function mod($number1 , $number2){
+    if( $number2 <= 0){
+        return "This case division is not possible ";
+    }else{
+        return($number1 % $number2);
+    }
+    
+}
+echo "<br>";
+echo "the result is ", mod(10, 0), "<br>";
+
+/// how to use try and catch block in program
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+try{
+    echo "Result: ", (7 % 3);
+}catch(Throwable $e ){
+    echo "Error: ", $e->getMessage() ;
+    echo "<br> Code: ", $e->getCode();
+    echo "<br> Line: ", $e->getLine();
+    echo "<br> File: ", $e->getFile();
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+try{
+    $nll = (70 / 0 );
+    echo "The result is = ", $nll . "<br>";
+}catch(t $e){
+    echo "<br> Line : ", $e->getLine(), "<br>";
+    echo "<br> Line : ", $e->getMessage(), "<br>";
+    echo "this part represent by catch part in the program";
+}
+
+
+echo "<h2>In this line are executed after try block executed </h2>";
+/*
+if( 40 % 0 ){
+    echo "this is not valid", "<br>";
+}else{
+    echo "this case are not executed";
+}
+*/
+
+echo "Again this line should be execute or not let's see what happed in line ", "<br>";
+echo "so twice ta eikhane bondhu seita hosse normally akhn aar any line execute hbe na cause this is an uncaught error so php engine are not continue run this statement so it's very interesting to learing";
+echo "That's why try and catch block are the most important to use amr bhai vlo lagse ";
+
+
+
+echo "ok<br><br>";
+
 
 
 ?>
